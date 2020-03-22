5418d566bc186f42a2fa4e4d0e879e1f8f27645c
Christopher Dreide <cdreide@protonmail.com>
2020-03-22 18:24:56 +0100

    fixed regex

--- script.js
+++ script.js
@@ -5,8 +5,8 @@ function js_function(value) {
     let output = window.location.href + '\n';
     let spans = document.getElementsByTagName('span');
     for(i = 0; i < spans.length; i++) {
-        if (spans[i].innerHTML.match(/^[a-z0-9]+$/)) {
-            let word = spans[i].innerHTML
+        if (spans[i].innerText.match(/^[a-zA-Z0-9]+/)) {
+            let word = spans[i].innerText
             let rect = spans[i].getBoundingClientRect()
             output += word + '\t(' + rect.left + ',' + rect.top + ',' + rect.width + ',' + rect.height + ')\n'
         }
